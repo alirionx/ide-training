2a8b24137d47dcdda39b105e68182798f249b4b4
Ubuntu <ubuntu@ip-172-31-27-180.eu-central-1.compute.internal>
2020-11-23 12:18:14 +0100

    with time

--- myapp.py
+++ myapp.py
@@ -4,6 +4,7 @@ import os
 from flask import Flask, request, render_template
 import json
 import sqlite3
+from datetime import datetime
 
 
 app = Flask(__name__)
@@ -12,14 +13,18 @@ app.debug = True
 appDir = os.path.dirname(os.path.realpath(__file__))
 
 
-@app.route('/hallowelt')
+@app.route('/')
 def html_hw():
+
+  now = datetime.now()
+  curTime = now.strftime("%H:%M:%S")
 
   name = 'Welt'
   if "name" in request.args:
     name =  request.args['name']
 
-  html_str = '<h2>Hallo %s</h2>' %name
+  html_str = '<h2>Hallo %s' %name
+  html_str += ' is is %s ' %curTime
 
   return html_str, 200 
 
